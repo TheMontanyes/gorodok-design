a274b20ddd33a985b5b2a67aae3d1dd1f1d70573
montanyes <themontanyes@yandex.ru>
2020-05-30 01:29:27 +0300

    component ready

--- src/components/Advantages/Advantages.vue
+++ src/components/Advantages/Advantages.vue
@@ -1,12 +1,46 @@
 <template>
-  <section class="advantages g-section">
+	<section class="advantages g-section">
 		<h2 class="g-main-title">Преимущества</h2>
-  </section>
+		<ul class="advantages-list">
+			<li class="advantages-item">
+				<img
+					class="advantages-item__img"
+					:src="require('@/assets/images/svg/opitraboty.svg')"
+					alt
+				/>
+				<p class="advantages-item__description">Хороший<br><span class="g-color-orange">опыт работы</span></p>
+			</li>
+			<li class="advantages-item">
+				<img
+					class="advantages-item__img"
+					:src="require('@/assets/images/svg/individualnost.svg')"
+					alt
+				/>
+				<p class="advantages-item__description"><span class="g-color-orange">Индивидуальный<br></span>подход к каждому клиенту</p>
+			</li>
+			<li class="advantages-item">
+				<img
+					class="advantages-item__img"
+					:src="require('@/assets/images/svg/sroki.svg')"
+					alt
+				/>
+				<p class="advantages-item__description"><span class="g-color-orange">Соблюдение<br></span>сроков работы</p>
+			</li>
+			<li class="advantages-item">
+				<img
+					class="advantages-item__img"
+					:src="require('@/assets/images/svg/professionalizm.svg')"
+					alt
+				/>
+				<p class="advantages-item__description"><span class="g-color-orange">Работа без<br></span>посредников</p>
+			</li>
+		</ul>
+	</section>
 </template>
 
 <script src="./Advantages.js"></script>
 
 <style lang="less">
-	@import "../../assets/css/settings.less";
-	@import './Advantages.less';
+@import "../../assets/css/settings.less";
+@import "./Advantages.less";
 </style>
